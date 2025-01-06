235335f46f645d9a8e553902280d4ae19eb8b278
김민지 <alswlk0306@gmail.com>
2025-01-06 10:58:08 +0900

    Chore: FE 개발환경 초기설정

---
+++ HalfFifty_FE/HalfFifty_FE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HalfFifty_FE
+//
+//  Created by 김민지 on 1/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
