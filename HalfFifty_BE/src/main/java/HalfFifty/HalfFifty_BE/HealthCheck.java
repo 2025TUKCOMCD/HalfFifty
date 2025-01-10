6204eb40b32be82e38996bbeddb83a6b6ed01150
yoosuhyeon <dbtngus20@naver.com>
2025-01-10 12:57:30 +0900

    Test : 로컬서버 HealthCheck

---
+++ HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/HealthCheck.java
@@ -0,0 +1,13 @@
+package HalfFifty.HalfFifty_BE;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/")
+    public String health() {
+        return "server on";
+    }
+}
