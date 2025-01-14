ce18aa060e169af6ac7f832a79a4bc902037fda4
jzongyun <jeong.yun@tukorea.ac.kr>
2025-01-14 16:33:59 +0900

    Feat: 스마트워치 개발 환경 세팅

---
+++ HalfFifty_FE/HalfFifty_Watch Watch App/HalfFifty_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalfFifty_WatchApp.swift
+//  HalfFifty_Watch Watch App
+//
+//  Created by 임정윤 on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct HalfFifty_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
