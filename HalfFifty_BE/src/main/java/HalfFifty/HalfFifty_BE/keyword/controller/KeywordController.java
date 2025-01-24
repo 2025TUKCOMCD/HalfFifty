c9c336c4a4bddd48b5c48aff9bdf349d1ee07bda
yoosuhyeon <dbtngus20@naver.com>
2025-01-25 00:43:41 +0900

    Feat : Keyword 등록 Controller 로직

---
+++ HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/keyword/controller/KeywordController.java
@@ -0,0 +1,43 @@
+package HalfFifty.HalfFifty_BE.keyword.controller;
+
+import HalfFifty.HalfFifty_BE.keyword.domain.DTO.RequestKeywordSaveDTO;
+import HalfFifty.HalfFifty_BE.keyword.service.KeywordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/keyword")
+public class KeywordController {
+    KeywordService keywordService;
+
+    @Autowired
+    public KeywordController(KeywordService keywordService) {
+        this.keywordService = keywordService;
+    }
+
+    @PostMapping
+    // 키워드 등록 API
+    public ResponseEntity<Map<String, Object>> saveKeyword(@RequestBody RequestKeywordSaveDTO requestKeywordSaveDTO) {
+        // 키워드 등록 service
+        UUID keywordId = keywordService.saveKeyword(requestKeywordSaveDTO);
+
+        // 키워드 생성 여부
+        boolean success = keywordId != null;
+
+        // Map을 통해 메시지와 id 값 json 데이터로 변환
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("success", success);
+        requestMap.put("message", success ? "키워드 등록 성공" : "키워드 생성 시 DAO 저장 실패");
+        requestMap.put("keywordId", keywordId);
+
+        // status, body 설정해서 응답 리턴
+        return ResponseEntity.status(HttpStatus.OK).body(requestMap);
+    }
+}
