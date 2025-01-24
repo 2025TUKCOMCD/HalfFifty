8c35944570309c708b09d184b1eed954a1124495
yoosuhyeon <dbtngus20@naver.com>
2025-01-25 01:09:57 +0900

    Feat : keyword 수정 로직 controller에 추가

--- HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/keyword/controller/KeywordController.java
+++ HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/keyword/controller/KeywordController.java
@@ -1,5 +1,6 @@
 package HalfFifty.HalfFifty_BE.keyword.controller;
 
+import HalfFifty.HalfFifty_BE.keyword.domain.DTO.RequestKeyWordUpdateDTO;
 import HalfFifty.HalfFifty_BE.keyword.domain.DTO.RequestKeywordSaveDTO;
 import HalfFifty.HalfFifty_BE.keyword.service.KeywordService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +23,8 @@ public class KeywordController {
         this.keywordService = keywordService;
     }
 
-    @PostMapping
     // 키워드 등록 API
+    @PostMapping
     public ResponseEntity<Map<String, Object>> saveKeyword(@RequestBody RequestKeywordSaveDTO requestKeywordSaveDTO) {
         // 키워드 등록 service
         UUID keywordId = keywordService.saveKeyword(requestKeywordSaveDTO);
@@ -35,6 +36,25 @@ public class KeywordController {
         Map<String, Object> requestMap = new HashMap<>();
         requestMap.put("success", success);
         requestMap.put("message", success ? "키워드 등록 성공" : "키워드 생성 시 DAO 저장 실패");
+        requestMap.put("keywordId", keywordId);
+
+        // status, body 설정해서 응답 리턴
+        return ResponseEntity.status(HttpStatus.OK).body(requestMap);
+    }
+
+    // 키워드 수정 API
+    @PutMapping
+    public ResponseEntity<Map<String, Object>> updateKeyword(@RequestBody RequestKeyWordUpdateDTO requestKeyWordUpdateDTO) {
+        // 키워드 수정 service
+        UUID keywordId = keywordService.updateKeyword(requestKeyWordUpdateDTO);
+
+        // 키워드 수정 여부
+        boolean success = keywordId != null;
+
+        // Map을 통해 메시지와 id 값 json 데이터로 변환
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("success", success);
+        requestMap.put("message", success ? "키워드 수정 성공" : "키워드 수정 시 DAO 저장 실패");
         requestMap.put("keywordId", keywordId);
 
         // status, body 설정해서 응답 리턴
