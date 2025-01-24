1de9659269b74f0dca47eafc5b422ff7d344a35a
yoosuhyeon <dbtngus20@naver.com>
2025-01-25 01:08:43 +0900

    Feat : keyword repository 쿼리 생성

--- HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/keyword/repository/KeywordRepositoryJPA.java
+++ HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/keyword/repository/KeywordRepositoryJPA.java
@@ -8,4 +8,5 @@ import java.util.UUID;
 
 @Repository
 public interface KeywordRepositoryJPA extends JpaRepository<KeywordDAO, UUID> {
+    KeywordDAO findByUserIdAndKeywordId(UUID userId, UUID keywordId);
 }
