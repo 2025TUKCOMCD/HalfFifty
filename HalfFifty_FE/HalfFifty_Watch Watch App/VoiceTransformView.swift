303960a42577dbeba4a6a2db9fa3c2d7638898ed
jzongyun <jeong.yun@tukorea.ac.kr>
2025-01-24 17:42:46 +0900

    Feat: 네비게이션을 이용해서 메인화면과 서브화면을 연결

--- HalfFifty_FE/HalfFifty_Watch Watch App/VoiceTransformView.swift
+++ HalfFifty_FE/HalfFifty_Watch Watch App/VoiceTransformView.swift
@@ -8,55 +8,45 @@ import SwiftUI
 
 struct VoiceTransformView: View {
     @State private var isRecording = false
-    
+
     var body: some View {
-        GeometryReader { geometry in
-            VStack(spacing: 8) {
+        VStack(spacing: 8) {
+            VStack {
                 HStack {
+                    Text(isRecording ? "녹음 중..." : "음성 버튼을 눌러주세요.")
+                        .foregroundColor(.gray)
+                        .font(.body)
+                        .padding()
                     Spacer()
-                    Text("음성변환")
-                        .font(.title2)
-                        .fontWeight(.semibold)
+                }
+                Spacer()
+            }
+            .frame(maxWidth: .infinity)
+            .background(Color.gray.opacity(0.3))
+            .cornerRadius(10)
+            
+            Button(action: {
+                isRecording.toggle()
+            }) {
+                ZStack {
+                    Circle()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.white)
+                    Image(systemName: isRecording ? "waveform" : "mic.fill")
                         .foregroundColor(.blue)
                 }
-                .frame(maxWidth: .infinity)
-                
-                VStack {
-                    HStack {
-                        Text(isRecording ? "녹음 중..." : "음성 버튼을 눌러주세요.")
-                            .foregroundColor(.gray)
-                            .font(.body)
-                            .padding()
-                        Spacer()
-                    }
-                    Spacer()
-                }
-                .frame(maxWidth: .infinity, minHeight: geometry.size.height * 0.75)
-                .background(Color.gray.opacity(0.3))
-                .cornerRadius(10)
-                
-                Button(action: {
-                    isRecording.toggle()
-                }) {
-                    ZStack {
-                        Circle()
-                            .frame(width: geometry.size.height * 0.16, height: geometry.size.height * 0.16)
-                            .foregroundColor(.white)
-                        Image(systemName: isRecording ? "waveform" : "mic.fill")
-                            .foregroundColor(.blue)
-                    }
-                }
-                .buttonStyle(PlainButtonStyle())
             }
-            .frame(width: geometry.size.width, height: geometry.size.height)
-            .background(Color.black.edgesIgnoringSafeArea(.all))
-            .foregroundColor(.white)
+            .buttonStyle(PlainButtonStyle())
         }
+        .padding()
+        .navigationTitle("음성변환")
     }
 }
 
 struct VoiceTransformView_Previews: PreviewProvider {
     static var previews: some View {
-        VoiceTransformView()
+        NavigationView {
+            VoiceTransformView()
+        }
     }
 }
