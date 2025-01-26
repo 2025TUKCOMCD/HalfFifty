16531c8c69883f4d15c77d38ff028c015389994e
yoosuhyeon <dbtngus20@naver.com>
2025-01-26 20:07:43 +0900

    Feat : keyword repository query 조건 설정

--- HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/keyword/repository/KeywordRepositoryJPA.java
+++ HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/keyword/repository/KeywordRepositoryJPA.java
@@ -4,9 +4,13 @@ import HalfFifty.HalfFifty_BE.keyword.domain.KeywordDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface KeywordRepositoryJPA extends JpaRepository<KeywordDAO, UUID> {
     KeywordDAO findByUserIdAndKeywordId(UUID userId, UUID keywordId);
+
+    // 유저 id를 통해 전체 키워드 객체들 조회
+    List<KeywordDAO> findAllByUserId(UUID userId);
 }
