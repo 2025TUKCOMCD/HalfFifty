9f428e40b7d8c6d3305ce2083c2228343a820de2
yoosuhyeon <dbtngus20@naver.com>
2025-01-26 18:41:56 +0900

    Feat : keyword 삭제 service

--- HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/keyword/service/KeywordService.java
+++ HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/keyword/service/KeywordService.java
@@ -1,8 +1,10 @@
 package HalfFifty.HalfFifty_BE.keyword.service;
 
+import HalfFifty.HalfFifty_BE.keyword.bean.DeleteKeywordBean;
 import HalfFifty.HalfFifty_BE.keyword.bean.SaveKeywordBean;
 import HalfFifty.HalfFifty_BE.keyword.bean.UpdateKeywordBean;
 import HalfFifty.HalfFifty_BE.keyword.domain.DTO.RequestKeyWordUpdateDTO;
+import HalfFifty.HalfFifty_BE.keyword.domain.DTO.RequestKeywordDeleteDTO;
 import HalfFifty.HalfFifty_BE.keyword.domain.DTO.RequestKeywordSaveDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,11 +15,13 @@ import java.util.UUID;
 public class KeywordService {
     SaveKeywordBean saveKeywordBean;
     UpdateKeywordBean updateKeywordBean;
+    DeleteKeywordBean deleteKeywordBean;
 
     @Autowired
-    public KeywordService(SaveKeywordBean saveKeywordBean, UpdateKeywordBean updateKeywordBean) {
+    public KeywordService(SaveKeywordBean saveKeywordBean, UpdateKeywordBean updateKeywordBean, DeleteKeywordBean deleteKeywordBean) {
         this.saveKeywordBean = saveKeywordBean;
         this.updateKeywordBean = updateKeywordBean;
+        this.deleteKeywordBean = deleteKeywordBean;
     }
 
     // 키워드 등록 API
@@ -28,5 +32,10 @@ public class KeywordService {
     // 키워드 수정 API
     public UUID updateKeyword(RequestKeyWordUpdateDTO requestKeyWordUpdateDTO) {
         return updateKeywordBean.exec(requestKeyWordUpdateDTO);
+    }
+
+    // 키워드 삭제 API
+    public Boolean deleteKeyword(RequestKeywordDeleteDTO requestKeywordDeleteDTO) {
+        return deleteKeywordBean.exec(requestKeywordDeleteDTO);
     }
 }
