3db8445aed35bce41ffee13556065436afb41b5d
김민지 <alswlk0306@gmail.com>
2025-02-05 00:19:10 +0900

    Design: 사용자 정보 페이지 퍼블리싱

---
+++ HalfFifty_FE/HalfFifty_FE/View/NicknameEditView.swift
@@ -0,0 +1,8 @@
+//
+//  NicknameEditView.swift
+//  HalfFifty_FE
+//
+//  Created by 김민지 on 2/5/25.
+//
+
+import Foundation
--- HalfFifty_FE/HalfFifty_FE/View/UserView.swift
+++ HalfFifty_FE/HalfFifty_FE/View/UserView.swift
@@ -5,4 +5,56 @@
 //  Created by 김민지 on 2/4/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        VStack {
+            // 사용자 정보 리스트
+            List {
+                Section {
+                    UserInfoRow(title: "이름", value: "유수현")
+                    UserInfoRow(title: "전화번호", value: "010-4148-8137")
+                    UserInfoRow(title: "계정 생성일", value: "2024.01.07")
+                }
+                
+                Section {
+                    NavigationLink(destination: NicknameEditView()) {
+                        HStack {
+                            Text("닉네임")
+                                .foregroundColor(.black)
+                            Spacer()
+                            Text("유수현")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+        }
+        .navigationTitle("사용자 정보")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+// 사용자 정보 행 뷰
+struct UserInfoRow: View {
+    let title: String
+    let value: String
+
+    var body: some View {
+        HStack {
+            Text(title)
+                .foregroundColor(.black)
+            Spacer()
+            Text(value)
+                .foregroundColor(.gray)
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        UserView()
+    }
+}
