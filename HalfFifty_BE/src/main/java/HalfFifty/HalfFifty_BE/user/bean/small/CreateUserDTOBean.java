471dfa38c15b8a6d90ffd7ca7f084a2c25bb96b4
yoosuhyeon <dbtngus20@naver.com>
2025-02-04 23:45:56 +0900

    Feat : 유저 프로필 API 로직

---
+++ HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/user/bean/small/CreateUserDTOBean.java
@@ -0,0 +1,18 @@
+package HalfFifty.HalfFifty_BE.user.bean.small;
+
+import HalfFifty.HalfFifty_BE.user.domain.DTO.ResponseUserGetDTO;
+import HalfFifty.HalfFifty_BE.user.domain.UserDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateUserDTOBean {
+    // 객체를 DTO로 변환해서 반환
+    public ResponseUserGetDTO exec(UserDAO userDAO) {
+        return ResponseUserGetDTO.builder()
+                .userId(userDAO.getUserId())
+                .nickname(userDAO.getNickName())
+                .phoneNumber(userDAO.getPhoneNumber())
+                .createdAt(userDAO.getCreatedAt())
+                .build();
+    }
+}
