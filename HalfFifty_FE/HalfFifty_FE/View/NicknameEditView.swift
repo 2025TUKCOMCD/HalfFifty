df206c9536546c57c22653c20634c7867dad5764
김민지 <alswlk0306@gmail.com>
2025-02-05 13:40:32 +0900

    Design: UserView에서 닉네임 바인딩 받아서 닉네임 변경하는 페이지 퍼블리싱

--- HalfFifty_FE/HalfFifty_FE/View/NicknameEditView.swift
+++ HalfFifty_FE/HalfFifty_FE/View/NicknameEditView.swift
@@ -5,4 +5,41 @@
 //  Created by 김민지 on 2/5/25.
 //
 
-import Foundation
+import SwiftUI
+
+// 닉네임 수정 화면
+struct NicknameEditView: View {
+    @Environment(\.presentationMode) var presentationMode // 뒤로 가기 기능을 위한 환경 변수
+    @Binding var nickname: String // 기존 닉네임 값
+
+    var body: some View {
+        Form {
+            Section(header: Text("닉네임")) {
+                TextField(nickname, text: $nickname)
+            }
+        }
+        .navigationBarTitle("닉네임", displayMode: .inline) // 중앙 정렬된 제목
+        .navigationBarItems(
+            trailing: Button(action: {
+                saveNickname()
+            }) {
+                Text("저장")
+                    .foregroundColor(.blue)
+            }
+        )
+        .background(Color(UIColor.systemGray6)) // 배경 색상 맞추기
+    }
+
+    // 닉네임 저장 함수 (API 연동)
+    private func saveNickname() {
+        print("닉네임 저장: \(nickname)")
+        presentationMode.wrappedValue.dismiss() // 저장 후 닫기
+    }
+}
+
+#Preview {
+    // 테스트용 프리뷰
+    NavigationView {
+        NicknameEditView(nickname: .constant("유고양"))
+    }
+}
