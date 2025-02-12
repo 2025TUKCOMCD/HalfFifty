1e2dc4441a1962406592ebdd55d23e8c4adad1d3
jzongyun <jeong.yun@tukorea.ac.kr>
2025-02-13 01:11:07 +0900

    Design: 키워드가 존재하지 않을 경우 페이지 작성

--- HalfFifty_FE/HalfFifty_FE/View/Keyword/KeywordSettingView.swift
+++ HalfFifty_FE/HalfFifty_FE/View/Keyword/KeywordSettingView.swift
@@ -45,40 +45,49 @@ struct KeywordSettingsView: View {
             VStack {
                 SearchBar(text: $searchText)
                     .padding(.top, 16)
-
                 if isLoading {
                     ProgressView("Loading...")
                 } else {
-                    List {
-                        ForEach(filteredKeywords) { keyword in
-                            HStack {
-                                Text(keyword.keyword)
-                                Spacer()
-                            }
-                            .contextMenu {
-                                NavigationLink(destination: KeywordAddView(keyword: keyword.keyword, keywordId: keyword.keywordId)) {
-                                    Label("편집", systemImage: "pencil")
+                    if keywords.isEmpty {
+                        VStack {
+                            Spacer()
+                            Text("키워드가 존재하지 않습니다.")
+                                .foregroundColor(.gray)
+                                .font(.caption)
+                            Spacer()
+                        }
+                    } else {
+                        List {
+                            ForEach(filteredKeywords) { keyword in
+                                HStack {
+                                    Text(keyword.keyword)
+                                    Spacer()
                                 }
-                                Button(role: .destructive) {
-                                    deleteKeyword(keywordId: keyword.keywordId)
-                                } label: {
-                                    Label("삭제", systemImage: "trash")
+                                .contextMenu {
+                                    NavigationLink(destination: KeywordAddView(keyword: keyword.keyword, keywordId: keyword.keywordId)) {
+                                        Label("편집", systemImage: "pencil")
+                                    }
+                                    Button(role: .destructive) {
+                                        deleteKeyword(keywordId: keyword.keywordId)
+                                    } label: {
+                                        Label("삭제", systemImage: "trash")
+                                    }
                                 }
-                            }
-                            .swipeActions(edge: .trailing) {
-                                Button("삭제") {
-                                    deleteKeyword(keywordId: keyword.keywordId)
+                                .swipeActions(edge: .trailing) {
+                                    Button("삭제") {
+                                        deleteKeyword(keywordId: keyword.keywordId)
+                                    }
+                                    .tint(.red)
+                                    
+                                    NavigationLink(destination: KeywordAddView(keyword: keyword.keyword, keywordId: keyword.keywordId)) {
+                                        Text("편집")
+                                    }
+                                    .tint(.gray)
                                 }
-                                .tint(.red)
-
-                                NavigationLink(destination: KeywordAddView(keyword: keyword.keyword, keywordId: keyword.keywordId)) {
-                                    Text("편집")
-                                }
-                                .tint(.gray)
                             }
                         }
+                        .listStyle(PlainListStyle())
                     }
-                    .listStyle(PlainListStyle())
                 }
             }
             .background(Color.white)
