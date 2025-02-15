9412e8a2d7edb6b4c18f94864073a0afe529f11f
yoosuhyeon <dbtngus20@naver.com>
2025-02-15 18:16:28 +0900

    Feat : AQ 삭제 service

--- HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/FAQ/service/AqService.java
+++ HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/FAQ/service/AqService.java
@@ -1,9 +1,11 @@
 package HalfFifty.HalfFifty_BE.FAQ.service;
 
 import HalfFifty.HalfFifty_BE.FAQ.bean.AqUserSaveBean;
+import HalfFifty.HalfFifty_BE.FAQ.bean.DeleteAqBean;
 import HalfFifty.HalfFifty_BE.FAQ.bean.GetAqsBean;
 import HalfFifty.HalfFifty_BE.FAQ.bean.UpdateAqAdminBean;
 import HalfFifty.HalfFifty_BE.FAQ.domain.DTO.RequestAqAdminUpdateDTO;
+import HalfFifty.HalfFifty_BE.FAQ.domain.DTO.RequestAqDeleteDTO;
 import HalfFifty.HalfFifty_BE.FAQ.domain.DTO.RequestAqUserSaveDTO;
 import HalfFifty.HalfFifty_BE.FAQ.domain.DTO.ResponseAqGetDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +19,14 @@ public class AqService {
     AqUserSaveBean aqUserSaveBean;
     UpdateAqAdminBean updateAqAdminBean;
     GetAqsBean getAqsBean;
-
+    DeleteAqBean deleteAqBean;
 
     @Autowired
-    public AqService(AqUserSaveBean aqUserSaveBean, UpdateAqAdminBean updateAqAdminBean, GetAqsBean getAqsBean) {
+    public AqService(AqUserSaveBean aqUserSaveBean, UpdateAqAdminBean updateAqAdminBean, GetAqsBean getAqsBean, DeleteAqBean deleteAqBean) {
         this.aqUserSaveBean = aqUserSaveBean;
         this.updateAqAdminBean = updateAqAdminBean;
         this.getAqsBean = getAqsBean;
+        this.deleteAqBean = deleteAqBean;
     }
 
     // AQ 전체조회
@@ -39,5 +42,10 @@ public class AqService {
     // AQ 어드민 답장
     public UUID updateAdminAq(RequestAqAdminUpdateDTO requestAqAdminUpdateDTO) {
         return updateAqAdminBean.exec(requestAqAdminUpdateDTO);
+    }
+
+    // AQ 삭제
+    public Boolean deleteAq(RequestAqDeleteDTO requestAqDeleteDTO) {
+        return deleteAqBean.exec(requestAqDeleteDTO);
     }
 }
