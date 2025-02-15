ae84e60da0ff138ef6030484ce7afc7131c550ca
yoosuhyeon <dbtngus20@naver.com>
2025-02-15 15:25:48 +0900

    Feat : AQ DAO 설정

---
+++ HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/FAQ/domain/AqDAO.java
@@ -0,0 +1,28 @@
+package HalfFifty.HalfFifty_BE.FAQ.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class AqDAO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    UUID aqId;
+    UUID userId;
+    String question;
+    String answer;
+    Boolean isAnswer;
+    LocalDateTime questionCreatedAt;
+    LocalDateTime answerCreatedAt;
+}
