419501fc00f7a83c34487c8448a55ddc75c049d5
yoosuhyeon <dbtngus20@naver.com>
2025-02-15 16:17:15 +0900

    Feat : AQ admin 답장 service

--- HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/FAQ/service/AqService.java
+++ HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/FAQ/service/AqService.java
@@ -1,6 +1,8 @@
 package HalfFifty.HalfFifty_BE.FAQ.service;
 
 import HalfFifty.HalfFifty_BE.FAQ.bean.AqUserSaveBean;
+import HalfFifty.HalfFifty_BE.FAQ.bean.UpdateAqAdminBean;
+import HalfFifty.HalfFifty_BE.FAQ.domain.DTO.RequestAqAdminUpdateDTO;
 import HalfFifty.HalfFifty_BE.FAQ.domain.DTO.RequestAqUserSaveDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,14 +12,21 @@ import java.util.UUID;
 @Service
 public class AqService {
     AqUserSaveBean aqUserSaveBean;
+    UpdateAqAdminBean updateAqAdminBean;
 
     @Autowired
-    public AqService(AqUserSaveBean aqUserSaveBean) {
+    public AqService(AqUserSaveBean aqUserSaveBean, UpdateAqAdminBean updateAqAdminBean) {
         this.aqUserSaveBean = aqUserSaveBean;
+        this.updateAqAdminBean = updateAqAdminBean;
     }
 
     // AQ 유저 등록
     public UUID saveUserAq(RequestAqUserSaveDTO requestAqUserSaveDTO) {
         return aqUserSaveBean.exec(requestAqUserSaveDTO);
+    }
+
+    // AQ 어드민 답장
+    public UUID updateAdminAq(RequestAqAdminUpdateDTO requestAqAdminUpdateDTO) {
+        return updateAqAdminBean.exec(requestAqAdminUpdateDTO);
     }
 }
