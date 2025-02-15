abee0781e3ea45da9c16fc5e83944361b1bf3d2b
yoosuhyeon <dbtngus20@naver.com>
2025-02-15 16:17:28 +0900

    Feat : AQ admin 답장 controller

--- HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/FAQ/controller/AqController.java
+++ HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/FAQ/controller/AqController.java
@@ -1,5 +1,6 @@
 package HalfFifty.HalfFifty_BE.FAQ.controller;
 
+import HalfFifty.HalfFifty_BE.FAQ.domain.DTO.RequestAqAdminUpdateDTO;
 import HalfFifty.HalfFifty_BE.FAQ.domain.DTO.RequestAqUserSaveDTO;
 import HalfFifty.HalfFifty_BE.FAQ.service.AqService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,24 @@ public class AqController {
         Map<String, Object> requestMap = new HashMap<>();
         requestMap.put("success", success);
         requestMap.put("message", success ? "AQ 유저 등록 성공" : "AQ 유저 등록 실패");
+        requestMap.put("AQId", AqId);
+
+        // status, body 값 설정해서 응답 리턴
+        return ResponseEntity.status(HttpStatus.OK).body(requestMap);
+    }
+
+    @PatchMapping
+    public ResponseEntity<Map<String, Object>> updateAdminAq(@RequestBody RequestAqAdminUpdateDTO requestAqAdminUpdateDTO) {
+        // AQ 어드민 답장 service
+        UUID AqId = aqService.updateAdminAq(requestAqAdminUpdateDTO);
+
+        // AQ 어드민 답장 성공 여부
+        Boolean success = AqId != null;
+
+        // Map을 통해 메시지와 list 값 json 데이터로 변환
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("success", success);
+        requestMap.put("message", success ? "AQ 어드민 답장 성공" : "AQ 어드민 답장 실패");
         requestMap.put("AQId", AqId);
 
         // status, body 값 설정해서 응답 리턴
