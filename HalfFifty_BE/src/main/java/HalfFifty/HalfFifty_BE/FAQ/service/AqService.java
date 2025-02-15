6a8413bf73417eb68dba33763ffb4b019d722441
yoosuhyeon <dbtngus20@naver.com>
2025-02-15 17:49:31 +0900

    Feat : AQ 전체조회 service

--- HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/FAQ/service/AqService.java
+++ HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/FAQ/service/AqService.java
@@ -1,23 +1,34 @@
 package HalfFifty.HalfFifty_BE.FAQ.service;
 
 import HalfFifty.HalfFifty_BE.FAQ.bean.AqUserSaveBean;
+import HalfFifty.HalfFifty_BE.FAQ.bean.GetAqsBean;
 import HalfFifty.HalfFifty_BE.FAQ.bean.UpdateAqAdminBean;
 import HalfFifty.HalfFifty_BE.FAQ.domain.DTO.RequestAqAdminUpdateDTO;
 import HalfFifty.HalfFifty_BE.FAQ.domain.DTO.RequestAqUserSaveDTO;
+import HalfFifty.HalfFifty_BE.FAQ.domain.DTO.ResponseAqGetDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class AqService {
     AqUserSaveBean aqUserSaveBean;
     UpdateAqAdminBean updateAqAdminBean;
+    GetAqsBean getAqsBean;
+
 
     @Autowired
-    public AqService(AqUserSaveBean aqUserSaveBean, UpdateAqAdminBean updateAqAdminBean) {
+    public AqService(AqUserSaveBean aqUserSaveBean, UpdateAqAdminBean updateAqAdminBean, GetAqsBean getAqsBean) {
         this.aqUserSaveBean = aqUserSaveBean;
         this.updateAqAdminBean = updateAqAdminBean;
+        this.getAqsBean = getAqsBean;
+    }
+
+    // AQ 전체조회
+    public List<ResponseAqGetDTO> getAqs(UUID userId) {
+        return getAqsBean.exec(userId);
     }
 
     // AQ 유저 등록
