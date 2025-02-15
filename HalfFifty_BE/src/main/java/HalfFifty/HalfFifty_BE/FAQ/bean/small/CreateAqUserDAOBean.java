57fef321cd94d802c024376a9e9750a32a28ac74
yoosuhyeon <dbtngus20@naver.com>
2025-02-15 15:26:56 +0900

    Feat : AQ 유저 등록 API 로직

---
+++ HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/FAQ/bean/small/CreateAqUserDAOBean.java
@@ -0,0 +1,20 @@
+package HalfFifty.HalfFifty_BE.FAQ.bean.small;
+
+import HalfFifty.HalfFifty_BE.FAQ.domain.AqDAO;
+import HalfFifty.HalfFifty_BE.FAQ.domain.DTO.RequestAqUserSaveDTO;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class CreateAqUserDAOBean {
+
+    // AQ 유저 객체 생성
+    public AqDAO exec(RequestAqUserSaveDTO requestAqUserSaveDTO) {
+        return AqDAO.builder()
+                .userId(requestAqUserSaveDTO.getUserId())
+                .question(requestAqUserSaveDTO.getQuestion())
+                .questionCreatedAt(LocalDateTime.now())
+                .build();
+    }
+}
