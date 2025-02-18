ef75d24e5398405cccd2993f1ece07b83375d794
jzongyun <jeong.yun@tukorea.ac.kr>
2025-02-18 12:51:43 +0900

    Feat: 튜토리얼 페이지 생성

---
+++ HalfFifty_FE/HalfFifty_FE/View/TutorialView.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialView.swift
+//  HalfFifty_FE
+//
+//  Created by 임정윤 on 2/18/25.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TutorialView()
+}
