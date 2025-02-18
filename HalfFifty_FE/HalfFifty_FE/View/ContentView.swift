0715dc76d3102a1617d0a4455fee0f13f76d8606
jzongyun <jeong.yun@tukorea.ac.kr>
2025-02-18 12:52:22 +0900

    Feat: 튜토리얼 뷰가 처음 앱을 실행시켰을 때만 나타나도록 설정

--- HalfFifty_FE/HalfFifty_FE/View/ContentView.swift
+++ HalfFifty_FE/HalfFifty_FE/View/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     // 메인 뷰 표시 여부
     @State private var showMainView = false
     
+    // 튜토리얼 뷰 표시 여부
+    @State private var showTutorialView = false
+    
     // 메뉴 표시 여부
     @State var showMenuView: Bool = false
 
@@ -31,22 +34,32 @@ struct ContentView: View {
             GeometryReader { geometry in
                 ZStack(alignment: .leading) {
                     if self.showMainView {
-                        // 메뉴 표시 여부 바인딩
-                        MainView(showMenuView: $showMenuView)
-                            .frame(width: geometry.size.width, height: geometry.size.height)
-                            .disabled(self.showMenuView) // 메뉴 표시 상태면 메인 뷰 비활성화
-                        
-                        if self.showMenuView {
-                            MenuView(showMenuView: $showMenuView, userViewModel: userViewModel) // ViewModel 전달
-                                .transition(.move(edge: .leading).combined(with: .opacity))
-                                .zIndex(2) // 항상 최상위에 있도록 설정
+                        if self.showTutorialView {
+                            TutorialView()
+                        } else {
+                            // 메뉴 표시 여부 바인딩
+                            MainView(showMenuView: $showMenuView)
+                                .frame(width: geometry.size.width, height: geometry.size.height)
+                                .disabled(self.showMenuView) // 메뉴 표시 상태면 메인 뷰 비활성화
+                            
+                            if self.showMenuView {
+                                MenuView(showMenuView: $showMenuView, userViewModel: userViewModel)
+                                    .transition(.move(edge: .leading).combined(with: .opacity))
+                                    .zIndex(2) // 항상 최상위에 있도록 설정
+                            }
                         }
                     } else {
                         SplashView()
                             .onAppear {
                                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
                                     withAnimation {
+                                        let isFirstLaunch = !UserDefaults.standard.bool(forKey: "hasLaunchedBefore")
+                                        self.showTutorialView = isFirstLaunch
                                         self.showMainView = true
+                                        
+                                        if isFirstLaunch {
+                                            UserDefaults.standard.set(true, forKey: "hasLaunchedBefore")
+                                        }
                                     }
                                 }
                                 userViewModel.fetchUser(userId: "9f373112-8e93-4444-a403-a986f8bea4a3")
