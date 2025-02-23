5e92a28453ac9206953a9a2d292da3a65affd1bc
yoosuhyeon <dbtngus20@naver.com>
2025-02-23 18:48:07 +0900

    Feat : 수화인식 API service

--- HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/translation/service/TranslationService.java
+++ HalfFifty_BE/src/main/java/HalfFifty/HalfFifty_BE/translation/service/TranslationService.java
@@ -1,26 +1,36 @@
 package HalfFifty.HalfFifty_BE.translation.service;
 
+import HalfFifty.HalfFifty_BE.translation.bean.FlaskSignLanguageBean;
 import HalfFifty.HalfFifty_BE.translation.bean.SaveTranslationBean;
 import HalfFifty.HalfFifty_BE.translation.domain.DTO.RequestSignLanguageDTO;
 import HalfFifty.HalfFifty_BE.translation.domain.DTO.ResponseTranslationGetDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 
 @Service
 public class TranslationService {
     SaveTranslationBean saveTranslationBean;
+    FlaskSignLanguageBean flaskSignLanguageBean;
 
     @Autowired
-    public TranslationService(SaveTranslationBean saveTranslationBean) {
+    public TranslationService(SaveTranslationBean saveTranslationBean, FlaskSignLanguageBean flaskSignLanguageBean) {
         this.saveTranslationBean = saveTranslationBean;
+        this.flaskSignLanguageBean = flaskSignLanguageBean;
     }
 
     public ResponseTranslationGetDTO signLanguageTranslation(RequestSignLanguageDTO requestSignLanguageDTO) {
-        // 람다를 통해 번역된 수화를 가져옴
+        Map<String, Object> aiResponse = flaskSignLanguageBean.exec(requestSignLanguageDTO);
 
-        // 번역된 수화를 저장하고 번역 기록을 프론트에게 전달
-        return saveTranslationBean.exec(requestSignLanguageDTO.getUserId(), "123");
+        if (aiResponse != null && Boolean.TRUE.equals(aiResponse.get("success"))) {
+            String translatedWord = (String) aiResponse.get("predicted_label");
+            Double probability = (Double) aiResponse.get("confidence");
+            return saveTranslationBean.exec(requestSignLanguageDTO.getUserId(), translatedWord, probability);
+        } else {
+            return null;
+        }
     }
 
 }
