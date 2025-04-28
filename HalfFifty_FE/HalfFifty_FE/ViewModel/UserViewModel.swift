5d7a59c4d3e9b18e7681894321bb21522aa113d2
김민지 <alswlk0306@gmail.com>
2025-04-29 02:52:28 +0900

    fix: 계정 생성일 데이터 마이크로초 제거

--- HalfFifty_FE/HalfFifty_FE/ViewModel/UserViewModel.swift
+++ HalfFifty_FE/HalfFifty_FE/ViewModel/UserViewModel.swift
@@ -89,8 +89,9 @@ class UserViewModel: ObservableObject {
     
     ///  서버 날짜 형식 (`2025-01-24T22:03:02.567423`) → "YYYY.MM.DD" 변환
     private func formatDate(_ dateString: String) -> String {
+        print(dateString)
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS" // 마이크로초 포함
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         formatter.locale = Locale(identifier: "ko_KR") // 한국어 로케일
         formatter.timeZone = TimeZone(abbreviation: "UTC") // 서버가 UTC 기반이라면 설정
         
