8272f4502dc0a4cc76bc53bb18b9557661fbb30e
김민지 <alswlk0306@gmail.com>
2025-04-29 15:53:08 +0900

    feat: 해석된 단어들 목록 출력되는 뷰

--- HalfFifty_FE/HalfFifty_FE/View/ChipsTranslationResultView.swift
+++ HalfFifty_FE/HalfFifty_FE/View/ChipsTranslationResultView.swift
@@ -5,4 +5,32 @@
 //  Created by 김민지 on 4/29/25.
 //
 
-import Foundation
+import SwiftUI
+
+public struct ChipsTranslationResultView: View {
+    let items: [String]
+    let horizontalSpacing: CGFloat
+    let verticalSpacing: CGFloat
+
+    public init(
+        items: [String],
+        horizontalSpacing: CGFloat = 8,
+        verticalSpacing: CGFloat = 8
+    ) {
+        self.items = items
+        self.horizontalSpacing = horizontalSpacing
+        self.verticalSpacing = verticalSpacing
+    }
+
+    public var body: some View {
+        GeometryReader { geometry in
+            FlexibleView(
+                availableWidth: geometry.size.width,
+                data: items,
+                spacing: horizontalSpacing
+            ) { item in
+                ChipsView(title: item)
+            }
+        }
+    }
+}
