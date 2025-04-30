8667fa3989a513b586d1210a905ee4885354fc0c
김민지 <alswlk0306@gmail.com>
2025-04-30 09:59:40 +0900

    style: 메뉴 목록 변경

---
+++ HalfFifty_FE/HalfFifty_Watch Watch AppTests/HalfFifty_Watch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HalfFifty_Watch_Watch_AppTests.swift
+//  HalfFifty_Watch Watch AppTests
+//
+//  Created by 김민지 on 4/29/25.
+//
+
+import Testing
+@testable import HalfFifty_Watch_Watch_App
+
+struct HalfFifty_Watch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
